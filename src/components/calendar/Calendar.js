37c4ee95ebf30e211dbbf30dffa624cf82124d75
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-06-26 11:31:18 -0400

    Get current month

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
 
 import "./calendar.css";
 
 const Calendar = () => {
-  return <div className="calendar">Calendar</div>;
+  const [date] = useState(moment());
+
+  return (
+    <div className="calendar">
+      <h2>{date.format("MMMM")}</h2>
+    </div>
+  );
 };
 
 export default Calendar;
