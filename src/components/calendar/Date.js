fdb14bdc5ca2f8fe65a0d98dfd3f7de616b12c58
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-06-26 14:53:14 -0400

    Create form to create events

--- src/components/calendar/Date.js
+++ src/components/calendar/Date.js
@@ -1,10 +1,33 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import ModalForm from "../ModalForm";
+
+const DATE_FORMAT = "MM/DD/YYYY";
 
 const Date = memo(({ date, currentMonth = true }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleSave = (values) => {};
+
   return (
-    <div className="date" style={{ color: currentMonth ? "black" : "#ccc" }}>
-      {date.date()}
-    </div>
+    <>
+      <div
+        className="date"
+        style={{ color: currentMonth ? "black" : "#ccc" }}
+        onClick={handleClick}
+      >
+        {date.date()}
+      </div>
+      <ModalForm
+        show={showModal}
+        date={date.format(DATE_FORMAT)}
+        onSave={handleSave}
+        onCancel={() => setShowModal(false)}
+      />
+    </>
   );
 });
 
