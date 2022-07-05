0236b4289b708c09d503ecdcd707bdd81e5ec943
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-07-05 14:14:05 -0400

    Create a calendar mock state

---
+++ __mocks-data__/calendarState.mock.js
@@ -0,0 +1,17 @@
+export const calendarState = {
+  calendar: {
+    current: { year: 2022, month: 7 },
+    dateRange: ['2022/06/26', '2022/08/07'],
+    events: {
+      '2022/07/04': [
+        {
+          date: '2022/07/04',
+          name: 'Independence Day',
+          start: '00:00:00',
+          end: '23:59:59',
+          description: 'American Independence day',
+        },
+      ],
+    },
+  },
+};
