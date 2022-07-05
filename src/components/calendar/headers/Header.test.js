1099e77e9ad8f3ec9896367a54d0a3626f0bb5bc
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-07-05 16:26:36 -0400

    Check for click event

--- src/components/calendar/headers/Header.test.js
+++ src/components/calendar/headers/Header.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { calendarState } from '__mocks-data__/calendarState.mock';
 import Header from './Header';
 
 const mockStore = configureStore([]);
+const dispatch = jest.fn();
 
 describe('Header tests', () => {
   test('should render current month', () => {
@@ -26,5 +28,24 @@ describe('Header tests', () => {
 
     expect(screen.getByText('Previous')).toBeInTheDocument();
     expect(screen.getByText('Next')).toBeInTheDocument();
+  });
+
+  test('should dispatch on click', () => {
+    const store = mockStore(calendarState);
+    store.dispatch = dispatch;
+
+    render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+
+    const previous = screen.getByText('Previous');
+    const next = screen.getByText('Next');
+
+    userEvent.click(previous);
+    userEvent.click(next);
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
   });
 });
